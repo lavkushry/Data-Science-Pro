1029d5e4e924f32fede47c9b9a1aa8fcc2e111a8
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 14:01:42 +0530

    added upper

---
+++ uppercase.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 14:00:30
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 14:01:14
+def upper(s):
+  return s.upper()
+
+print(upper("Lavkush"))
