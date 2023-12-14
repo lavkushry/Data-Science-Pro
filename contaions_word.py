f57c10aeab448ac9e5856770f35468c3985a50c5
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 15:48:24 +0530

    added contains_word

---
+++ contaions_word.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 15:47:51
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 15:48:00
+def contaions_word(s,w):
+  return (w in s)
+
+print(contaions_word('lavkush','lav'))
