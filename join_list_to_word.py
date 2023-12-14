5b352477d456ec4200c2d4c79910c06450b693df
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 16:13:32 +0530

    added join_list_to_word

---
+++ join_list_to_word.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 16:10:44
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 16:13:02
+def join_list_to_word(lis):
+  word=''
+  return (word.join(lis))
+print(join_list_to_word(['a','e','i','o','u']))
