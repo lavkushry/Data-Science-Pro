bc891d63d4cc4586f2a9c32ba5e31dc5fb2878d4
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 16:01:48 +0530

    find first ocuurance

---
+++ ind_first_occurance.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 16:01:02
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 16:01:14
+def find_first_occurance(s,w):
+  first_occurace=s.find(w)
+  return first_occurace
+
+print(find_first_occurance('lalalalla','l'))
