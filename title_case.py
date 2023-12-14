881f3f1849187a28cb1f88a260430d61c6a110ac
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 21:06:21 +0530

    added Convert into title case

---
+++ title_case.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 21:04:59
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 21:05:10
+def title_case(s):
+  return s.title()
+
+print(title_case("hello world, welcome to python programming."))
