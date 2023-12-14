384877b6fa69430f974ac47303f0c1b39114fc04
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 13:43:17 +0530

    First Commit

---
+++ j.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 13:42:25
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 13:42:32
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
