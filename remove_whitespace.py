b94e194e3ff94b7b2c1b7b050ce32e664685fc8e
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 14:20:06 +0530

    remove all white spaces

---
+++ remove_whitespace.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 14:19:37
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 14:19:40
+def remove_whitespace(s):
+  return(s.strip())
+
+print(remove_whitespace("    Lavkush   "))
