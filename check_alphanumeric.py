b2fa3c965abc66726c0a45bf00ffb80182a94d3e
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 21:50:16 +0530

    added check alphanumeric

---
+++ check_alphanumeric.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 21:49:36
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 21:49:40
+def check_alphanumeric(s):
+  return s.isalnum()
+
+print(check_alphanumeric("Lavkush"))
