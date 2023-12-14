b0f84727c3a088ce9acbf41835d102948cb6a5be
Lavkush Kumar <lavkushry@gmail.com>
2023-12-14 16:08:18 +0530

    string split to list

---
+++ split_string_to_list.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Roni Laukkarinen
+# @Date:   2023-12-14 16:07:50
+# @Last Modified by:   Roni Laukkarinen
+# @Last Modified time: 2023-12-14 16:07:54
+def split_string_to_list(s):
+  lis=[]
+  for i in s:
+    lis.append(i)
+  return lis
+
+print(split_string_to_list('LAVKUSH'))
